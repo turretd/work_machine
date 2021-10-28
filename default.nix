0a41753830cf2c76f7c897866a2bd11b017b9a24
turretd <turretd@noreply.users.github.com>
2021-10-28 09:18:09 +0200

    rework default.nix to also have nixpkgs includes

--- default.nix
+++ default.nix
@@ -1,8 +1,7 @@
-let
-  work_machine = (import <nixpkgs/nixos> {
+{sources ? import ./nix/sources.nix { }, pkgs ? import sources.nixpkgs {}}:
+{
+  machine = (import "${sources.nixpkgs}/nixos" {
     system = "x86_64-linux";
     configuration = import ./configuration.nix;
   }).system;
-in {
-  machine = { inherit work_machine; };
 }
